04b6c3fd5f78191b71cf7c87a21d637e01a18825
Divyam <divyam09srivastava@gmail.com>
2021-01-31 18:10:13 +0530

    item added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import './Header.css'
 import TeslaLogo from '../assets/teslaLogoSmall.svg'
 const Header =() => {
---
+++ src/components/item.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Button from './Button'
+import './item.css'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+
+const Item = ({title, desc, backgroundImg, leftBtnTxt, leftBtnLink,rightBtnTxt, rightBtnLink, twoButtons, first}) => {
+    return (
+        <div className="item" style={{
+            backgroundImage : `url(${backgroundImg})`
+        }}>
+            <div className="item__container">
+                <div className="item__text">
+                    <p>{title}</p>
+                    <div className="item__textDesc">
+                        <p>{desc}</p>
+                    </div>
+                </div>
+
+                <div className="item__lowerThird">
+                    <div className="item__buttons">
+                        <Button imp='primary' text={leftBtnTxt} link ={leftBtnLink} />
+                        { twoButtons &&(
+                            <Button imp='secondary' text={rightBtnTxt} link={rightBtnLink} />
+                        
+                        )}
+                        {first &&(
+                            <div className="item__expand">
+                                <ExpandMoreIcon/>
+                            </div>
+                        )
+
+                        }
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Item
